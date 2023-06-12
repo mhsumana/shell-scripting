457426ad71351ad4fadfe9018de14be82948ba22
sumana <mhsumana@gmail.com>
2023-06-12 22:19:08 +1000

    Auto Push

--- 04-dynamicVars.sh
+++ 04-dynamicVars.sh
@@ -3,6 +3,6 @@
 # TODAYS_DATE="12JUNE2023"
 TODAYS_DATE=$(date +%F)
 NO_OF_SESSIONS=$(who | wc -l)
-echo -e "Good morning and todays date is /e[32m $TODAYS_DATE \e[0m"
-echo -e "Number of open sessions is /e[33m $NO_OF_SESSIONS \e[0m"
+echo -e "Good morning and todays date is \e[32m $TODAYS_DATE \e[0m"
+echo -e "Number of open sessions is \e[33m $NO_OF_SESSIONS \e[0m"
 
