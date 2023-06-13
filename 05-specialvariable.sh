b40d28cd6da67fd1fa0ff2f2df5bb848244cac93
sumana <mhsumana@gmail.com>
2023-06-13 15:38:33 +1000

    Auto Push

---
+++ 05-specialvariable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Special variable gives special properties to your variables
+
+# Here are few of the special variable : $0 to $9, $?, $#, $*, $@
+
+echo $0 # $0 prints the script name you are exceuting
+echo "Name of the script executed is $0"
